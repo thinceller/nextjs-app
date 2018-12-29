7f46c9e01a87b5f885f749c09f13a9877920ca29
thinceller <kohei.ct@icloud.com>
2018-12-29 23:17:15 +0900

    add first routing
    add about page and link in index page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>Welcome to next.js!</div>
+import Link from 'next/link'
+
+export default () => (
+  <div>
+    Click{' '}
+    <Link href="/about">
+      <a>here</a>
+    </Link>{' '}
+    to read more
+  </div>
+)
